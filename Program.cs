964a1c2408b4c5802552d31043029714cf05a449
kimamo wachira <kimamo@live.com>
2018-05-28 22:30:38 -0400

    Initial check-in of module Blockchain

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Kimamo.Blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Blockchain zamudaCoin = new Blockchain();
+            zamudaCoin.AddBlock(new Block(DateTime.UtcNow, null, "{sender:Kibunja,receiver:Kimamo,amount:250}")); //Kibunja --> Kimamo
+            zamudaCoin.AddBlock(new Block(DateTime.UtcNow, null, "{sender:Kimamo,receiver:Kibunja,amount:50}"));  // Kimamo --> Kibunja
+            zamudaCoin.AddBlock(new Block(DateTime.UtcNow, null, "{sender:Kimamo,receiver:Kibunja,amount:150}"));  // Kibunja --> Kimamo
+
+            Console.WriteLine(JsonConvert.SerializeObject(zamudaCoin, Formatting.Indented));
+
+            Console.WriteLine($"Is Chain Valid: {zamudaCoin.IsValid()}");
+
+            Console.WriteLine($"Update amount to 10000");
+            zamudaCoin.Chain[1].Data = "{sender:Kibunja,receiver:Kimamo,amount:5000}";
+
+            Console.WriteLine($"Is Chain Valid: {zamudaCoin.IsValid()}");
+
+            Console.WriteLine($"Update hash");
+            zamudaCoin.Chain[1].Hash = zamudaCoin.Chain[1].CalculateHash();
+
+            Console.WriteLine($"Is Chain Valid: {zamudaCoin.IsValid()}");
+
+            Console.WriteLine($"Update the entire chain");
+            zamudaCoin.Chain[2].PreviousHash = zamudaCoin.Chain[1].Hash;
+            zamudaCoin.Chain[2].Hash = zamudaCoin.Chain[2].CalculateHash();
+            zamudaCoin.Chain[3].PreviousHash = zamudaCoin.Chain[2].Hash;
+            zamudaCoin.Chain[3].Hash = zamudaCoin.Chain[3].CalculateHash();
+
+            Console.WriteLine($"Is Chain Valid: {zamudaCoin.IsValid()}");
+
+            Console.ReadKey();
+        }
+    }
+}
